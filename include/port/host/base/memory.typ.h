3b300d050e47ad7c03fd7c725161762b76de8b9a
ivanp7 <ivanp7@mail.ru>
2024-06-17 09:03:06 +0300

    feat(host/base): split data storage types into a separate header, add storage representation in memory

--- include/port/host/base/memory.typ.h
+++ include/port/host/base/memory.typ.h
@@ -28,10 +28,6 @@
 
 #include <port/cdev/memory.typ.h>
 
-///////////////////////////////////////////////////////////////////////////////
-// Memory operations
-///////////////////////////////////////////////////////////////////////////////
-
 /**
  * @brief Memory allocator function.
  *
@@ -71,70 +67,6 @@ typedef port_bool_t (*port_memory_unmap_func_t)(
         port_void_ptr_t memory, ///< [in] Memory to unmap.
         port_void_ptr_t properties ///< [in] Memory unmapping properties.
 );
-
-///////////////////////////////////////////////////////////////////////////////
-// Data storage
-///////////////////////////////////////////////////////////////////////////////
-
-/**
- * @brief Data storage header.
- */
-typedef struct port_data_storage_header {
-    port_uint_single_t format;    ///< Description of the data storage format.
-    port_uint_single_t full_size; ///< Full size of the data storage.
-
-    struct {
-        port_uint_single_t num_entries;    ///< Number of entries in a table.
-        port_uint_single_t entries_offset; ///< Offset of table entries.
-    } string_table,     ///< Strings.
-        section_table,  ///< Data sections.
-        symbol_table,   ///< Symbols (references into sections).
-        property_table; ///< Properties (named values).
-
-    struct {
-        port_uint_single_t contents_size;   ///< Size of contents.
-        port_uint_single_t contents_offset; ///< Offset to contents.
-    } strings,      ///< Strings.
-        sections,   ///< Data sections.
-        properties; ///< Properties.
-} port_data_storage_header_t;
-
-/**
- * @brief Entry of data storage string table.
- */
-typedef struct port_data_storage_string_table_entry {
-    port_uint_single_t length; ///< String length.
-    port_uint_single_t offset; ///< String offset.
-} port_data_storage_string_table_entry_t;
-
-/**
- * @brief Entry of data storage section table.
- */
-typedef struct port_data_storage_section_table_entry {
-    port_uint_single_t name_str_idx; ///< String table index of section name string.
-    port_uint_single_t size;         ///< Section size.
-    port_uint_single_t offset;       ///< Section offset.
-} port_data_storage_section_table_entry_t;
-
-/**
- * @brief Entry of data storage symbol table.
- *
- * Symbols have no contents.
- */
-typedef struct port_data_storage_symbol_table_entry {
-    port_uint_single_t name_str_idx; ///< String table index of symbol name string.
-    port_uint_single_t section_idx;  ///< Index of section the symbol is relative to.
-    port_uint_single_t value;        ///< Symbol offset relative to the section beginning.
-} port_data_storage_symbol_table_entry_t;
-
-/**
- * @brief Entry of data storage property table.
- */
-typedef struct port_data_storage_property_table_entry {
-    port_uint_single_t name_str_idx; ///< String table index of property name string.
-    port_uint_single_t size;         ///< Property value size.
-    port_uint_single_t offset;       ///< Property value offset.
-} port_data_storage_property_table_entry_t;
 
 #endif // _PORT_HOST_BASE_MEMORY_TYP_H_
 
---
+++ include/port/host/base/storage.typ.h
@@ -0,0 +1,124 @@
+/****************************************************************************
+ * Copyright (C) 2020-2024 by Ivan Podmazov                                 *
+ *                                                                          *
+ * This file is part of Port.                                               *
+ *                                                                          *
+ *   Port is free software: you can redistribute it and/or modify it        *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Port is distributed in the hope that it will be useful,                *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Port. If not, see <http://www.gnu.org/licenses/>.   *
+ ****************************************************************************/
+
+/**
+ * @file
+ * @brief Data storage format.
+ */
+
+#pragma once
+#ifndef _PORT_HOST_BASE_STORAGE_TYP_H_
+#define _PORT_HOST_BASE_STORAGE_TYP_H_
+
+#include <port/cdev/memory.typ.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Storage representation in memory
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Data storage in memory.
+ */
+typedef struct port_data_storage {
+    port_uint_single_t format; ///< Description of the data storage format.
+
+    port_uint_single_t num_sections;   ///< Number of sections.
+    port_void_ptr_t *sections;         ///< Sections contents.
+    port_uint_single_t *section_sizes; ///< Sizes of sections.
+    char **section_names;              ///< Names of sections.
+
+    port_uint_single_t num_symbols;      ///< Number of symbols.
+    port_uint_single_t *symbol_sections; ///< Indices of sections symbols belong to.
+    port_uint_single_t *symbol_offsets;  ///< Offsets of symbols.
+
+    port_uint_single_t num_properties;        ///< Number of properties.
+    char **property_names;                    ///< Names of properties.
+    port_void_ptr *property_values;           ///< Values of properties.
+    port_uint_single_t *property_value_sizes; ///< Sizes of properties values.
+
+    port_void_ptr strings; ///< Common memory for all strings of the storage.
+    port_void_ptr properties; ///< Common memory for all properties values of the storage.
+} port_data_storage_t;
+
+///////////////////////////////////////////////////////////////////////////////
+// Storage file format
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Data storage header.
+ */
+typedef struct port_data_storage_file_header {
+    port_uint_single_t format;    ///< Description of the data storage format.
+    port_uint_single_t full_size; ///< Full size of the data storage.
+
+    struct {
+        port_uint_single_t num_entries;    ///< Number of entries in a table.
+        port_uint_single_t entries_offset; ///< Offset of table entries.
+    } string_table,     ///< Strings.
+        section_table,  ///< Data sections.
+        symbol_table,   ///< Symbols (references into sections).
+        property_table; ///< Properties (named values).
+
+    struct {
+        port_uint_single_t contents_size;   ///< Size of contents.
+        port_uint_single_t contents_offset; ///< Offset to contents.
+    } strings,      ///< Strings.
+        sections,   ///< Data sections.
+        properties; ///< Properties.
+} port_data_storage_file_header_t;
+
+/**
+ * @brief Entry of data storage string table.
+ */
+typedef struct port_data_storage_file_string_table_entry {
+    port_uint_single_t length; ///< String length.
+    port_uint_single_t offset; ///< String offset.
+} port_data_storage_file_string_table_entry_t;
+
+/**
+ * @brief Entry of data storage section table.
+ */
+typedef struct port_data_storage_file_section_table_entry {
+    port_uint_single_t name_str_idx; ///< String table index of section name string.
+    port_uint_single_t size;         ///< Section size.
+    port_uint_single_t offset;       ///< Section offset.
+} port_data_storage_file_section_table_entry_t;
+
+/**
+ * @brief Entry of data storage symbol table.
+ *
+ * Symbols have no contents.
+ */
+typedef struct port_data_storage_file_symbol_table_entry {
+    port_uint_single_t name_str_idx; ///< String table index of symbol name string.
+    port_uint_single_t section_idx;  ///< Index of section the symbol is relative to.
+    port_uint_single_t value;        ///< Symbol offset relative to the section beginning.
+} port_data_storage_file_symbol_table_entry_t;
+
+/**
+ * @brief Entry of data storage property table.
+ */
+typedef struct port_data_storage_file_property_table_entry {
+    port_uint_single_t name_str_idx; ///< String table index of property name string.
+    port_uint_single_t size;         ///< Property value size.
+    port_uint_single_t offset;       ///< Property value offset.
+} port_data_storage_file_property_table_entry_t;
+
+#endif // _PORT_HOST_BASE_STORAGE_TYP_H_
+
